62cf763439ce62f5c4f2b2b08a10f2c0ea8c90bf
Marlon Lückert <m.lueckert@me.com>
2020-04-19 21:23:33 +0200

    added barba

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,1 +1,43 @@
 import './vh';
+import barba from '@barba/core';
+import anime from 'animejs';
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+barba.init({
+    transitions: [{
+      sync: true, // sync the transitions up so they occur concurrently
+      leave({ // leaving the current page
+        current,
+      }) {
+        const targets = current.container; // target current page
+        const a = anime({ // new animation
+          targets, // current page
+          translateX: [0, "100%"], // fade out from 1 to 0 opacity
+          duration: 800, //time in ms
+          easing: 'easeInOutExpo'
+        });
+  
+        return a.finished;
+      },
+      enter({ // entering the next page
+        next,
+      }) {
+        const targets = next.container; // target the container of the next page
+        targets.style.position = "fixed";
+        targets.style.top = targets.style.bottom = targets.style.left = targets.style.right = 0;
+        const a = anime({ // new animation
+          targets, // next page
+          translateX: ["-100%", 0], // fade in from 0 to 1 opacity
+          duration: 800, // time in ms
+          easing: 'easeInOutExpo'
+        });
+  
+        return a.finished.then(() => {
+          targets.style.position = "relative";
+        });
+      },
+    }],
+  });
+  });
+
