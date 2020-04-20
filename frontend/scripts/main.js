78b94a0fde0ebe577b004985d65a99c2125a8398
Marlon Lückert <m.lueckert@me.com>
2020-04-20 19:41:05 +0200

    added page transition

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,1 +1,2 @@
 import './vh';
+import './transitions';
---
+++ frontend/scripts/transitions.js
@@ -0,0 +1,52 @@
+import barba from '@barba/core';
+import anime from 'animejs';
+
+document.addEventListener("DOMContentLoaded", function () {
+    barba.init({
+        transitions: [{
+            sync: true,
+            leave({ current, trigger }) {
+
+                let translateX = [0, "-100%"];
+                if (trigger.dataset["direction"] == "forward") {
+                    translateX = [0, "-100%"];
+                } else if (trigger.dataset["direction"] == "backward") {
+                    translateX = [0, "100%"];
+                }
+
+                const targets = current.container;
+                const a = anime({
+                    targets,
+                    translateX,
+                    duration: 800,
+                    easing: 'easeInOutExpo'
+                });
+
+                return a.finished;
+            },
+            enter({ next, trigger }) {
+
+                let translateX = ["100%", 0];
+                if (trigger.dataset["direction"] == "forward") {
+                    translateX = ["100%", 0];
+                } else if (trigger.dataset["direction"] == "backward") {
+                    translateX = ["-100%", 0];
+                }
+
+                const targets = next.container;
+                targets.style.position = "fixed";
+                targets.style.top = targets.style.bottom = targets.style.left = targets.style.right = 0;
+                const a = anime({
+                    targets,
+                    translateX,
+                    duration: 800,
+                    easing: 'easeInOutExpo'
+                });
+
+                return a.finished.then(() => {
+                    targets.style.position = "relative";
+                });
+            },
+        }],
+    });
+});
