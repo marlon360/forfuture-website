d31aafdb96de46acc34a1bd72757f6eb73d8d5d9
Marlon Lückert <m.lueckert@me.com>
2020-04-24 11:29:06 +0200

    more options on page 4

--- frontend/src/pages/survey-04.page.js
+++ frontend/src/pages/survey-04.page.js
@@ -6,12 +6,17 @@ import { useLocalStorage } from '../utils/localStorage';
 
 function Survey04Page() {
 
+    const [democracy, setDemocracy] = useLocalStorage("democracy", 3);
+    const [money, setMoney] = useLocalStorage("money", 3);
     const [success, setSuccess] = useLocalStorage("success", 3);
     const [education, setEducation] = useLocalStorage("education", 3);
     const [health, setHealth] = useLocalStorage("health", 3);
     const [environment, setEnvironment] = useLocalStorage("environment", 3);
     const [family, setFamily] = useLocalStorage("family", 3);
 
+    const onChangeMoney = (evt) => {
+        setMoney(evt.value);
+    }
     const onChangeSuccess = (evt) => {
         setSuccess(evt.value);
     }
@@ -27,15 +32,20 @@ function Survey04Page() {
     const onChangeFamily = (evt) => {
         setFamily(evt.value);
     }
+    const onChangeDemocracy = (evt) => {
+        setDemocracy(evt.value);
+    }
 
     return (
         <SurveyFrame number="04/12" question="Wie wichtig sind diese Dinge für deine persönliche Zukunft" prevPath="/03" nextPath="/05">
             <p>Bewerte: 1 = kaum, 5 = sehr wichtig!</p>
+            <SliderComponent title="Geld" onChange={onChangeMoney} min={1} max={5} value={money}></SliderComponent>
             <SliderComponent title="Erfolg" onChange={onChangeSuccess} min={1} max={5} value={success}></SliderComponent>
-            <SliderComponent title="Bildung" onChange={onChangeEducation} min={1} max={5} value={education}></SliderComponent>
             <SliderComponent title="Gesundheit" onChange={onChangeHealth} min={1} max={5} value={health}></SliderComponent>
             <SliderComponent title="Umwelt" onChange={onChangeEnvironment} min={1} max={5} value={environment}></SliderComponent>
             <SliderComponent title="Freunde/Famile" onChange={onChangeFamily} min={1} max={5} value={family}></SliderComponent>
+            <SliderComponent title="Demokratie" onChange={onChangeDemocracy} min={1} max={5} value={democracy}></SliderComponent>
+            <SliderComponent title="Bildung" onChange={onChangeEducation} min={1} max={5} value={education}></SliderComponent>
         </SurveyFrame>
     );
 }
