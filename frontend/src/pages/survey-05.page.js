97813283a72104d9cc44c03e9ab9b36bb87dfd32
Marlon Lückert <m.lueckert@me.com>
2020-04-26 17:08:36 +0200

    underline

--- frontend/src/pages/survey-05.page.js
+++ frontend/src/pages/survey-05.page.js
@@ -15,7 +15,7 @@ function Survey05Page() {
     }
 
     return (
-        <SurveyFrame number="05/12" question="WIE BEWERTEST DU DIESE DINGE IN HINBLICK AUF DIE ZUKUNFT IN DEUTSCHLAND?" prevPath="/04" nextPath="/06">
+        <SurveyFrame number="05/12" question={<>WIE BEWERTEST <span class="underline">DU</span> DIESE DINGE IN HINBLICK AUF DIE ZUKUNFT IN DEUTSCHLAND?</>} prevPath="/04" nextPath="/06">
             <p>Ordne nach Priorität.<br/>
             Die wichtigste Priorität steht an oberster Stelle.</p>
             <DndListComponent onChange={onChange} items={futureList}></DndListComponent>
