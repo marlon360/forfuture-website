78541193e7a3ad7ad406ccbec7cb9e5f44e544bd
Marlon Lückert <m.lueckert@me.com>
2020-04-27 13:45:15 +0200

    submit frontend

--- frontend/src/services/submit.service.js
+++ frontend/src/services/submit.service.js
@@ -3,8 +3,23 @@ export function Submit() {
         let saved = window.localStorage.getItem("for-future-state");
         if (saved) {
             let state = JSON.parse(saved);
-            console.log(state);
-            resolve();
+            
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(state)
+            };
+            fetch('/api/submit/', requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        resolve(data);
+                    } else {
+                        reject(data);
+                    }
+                })
+                .catch(error => reject(error))
+
         } else {
             reject("Du musst vorher alle Felder ausfüllen!");
         }
