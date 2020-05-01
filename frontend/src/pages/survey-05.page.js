cb3a06926e333e79f81082bbe8f2754c19dd156c
Marlon Lückert <m.lueckert@me.com>
2020-05-01 11:06:08 +0200

    sorting text

--- frontend/src/pages/survey-05.page.js
+++ frontend/src/pages/survey-05.page.js
@@ -17,8 +17,9 @@ function Survey05Page() {
     return (
         <SurveyFrame number="05/12" question={<>WIE BEWERTEST <span class="underline">DU</span> DIESE DINGE IN HINBLICK AUF DIE ZUKUNFT IN DEUTSCHLAND?</>} prevPath="/04" nextPath="/06">
             <p>Ordne nach Priorität.<br/>
-            Die wichtigste Priorität steht an oberster Stelle.</p>
+            Das Wichtigste steht an oberster Stelle.</p>
             <DndListComponent onChange={onChange} items={futureList}></DndListComponent>
+            <p style={{"font-size": "11px", "margin-top": "20px"}}>*Manchmal kann es zu <span className="underline">technischen Problemen</span> kommen. Du kannst die Tabs verschieben, in dem du sie immer von Oben nach Unten ziehst.</p>
         </SurveyFrame>
     );
 }
