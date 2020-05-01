14d8a7b504b5f5f9fe1861717db3082d704fd1e5
Marlon Lückert <m.lueckert@me.com>
2020-05-01 11:03:24 +0200

    landing wording

--- frontend/src/pages/landing.page.js
+++ frontend/src/pages/landing.page.js
@@ -36,9 +36,9 @@ function LandingPage() {
                 <div className="top">
                     <span className="title">HEY!</span>
                     <p>
-                    Das Ausfüllen dauert ungefähr <span className="underline">zehn Minuten</span>. Bitte beantworte die Fragen ehrlich! Sie sind wissenschaftliche Grundlage einer Bachelorarbeit namens »_forfuture». »_forfuture» geht um die Präsenz von Zukunft und stärkerer Integierung von Nachhaltigkeit in unseren alltäglichen Routinen. <br/>
+                    Das Ausfüllen dauert ungefähr <span className="underline">zehn Minuten</span>. Bitte beantworte die Fragen ehrlich! Sie sind wissenschaftliche Grundlage einer Bachelorarbeit namens »_forfuture». In »_forfuture» geht es um die Präsenz von Zukunft und stärkerer Integierung von Nachhaltigkeit in unsere täglichen Routinen. <br/>
                     <br/>
-                    Vor allem DEINE Meinung ist gefragt, es gibt also <span className="underline">kein</span> Richtig oder Falsch.
+                    DEINE Meinung ist gefragt, es gibt also <span className="underline">kein</span> Richtig oder Falsch.
                     </p>
                 </div>
                 <div className="bottom">
