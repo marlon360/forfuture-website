57e46c905d369423e3f05fc3b6859dd8c9afcc64
Marlon Lückert <m.lueckert@me.com>
2020-05-03 23:07:02 +0200

    delete answers of not used path

--- frontend/src/services/submit.service.js
+++ frontend/src/services/submit.service.js
@@ -3,6 +3,7 @@ export function Submit() {
         let saved = window.localStorage.getItem("for-future-state");
         if (saved) {
             let state = JSON.parse(saved);
+            state = cleanState(state);
             
             const requestOptions = {
                 method: 'POST',
@@ -24,4 +25,38 @@ export function Submit() {
             reject("Du musst vorher alle Felder ausfüllen!");
         }
     });
+}
+
+
+function cleanState(state) {
+
+    if (state.future == "Nein") {
+        state.democracy = null;
+        state.money = null;
+        state.success = null;
+        state.education = null;
+        state.health = null;
+        state.environment = null;
+        state.family = null;
+        state.futurelist = null;
+        state.changefuture = null;
+        state.climatechange = null;
+    }
+
+    if (state.sustainability == "Nein") {
+        state.sustainabilityscale = null;
+    }
+    if (state.sustainability == "Ja") {
+        state.nosustainability = null;
+    }
+
+    if (state.sustainabilityeveryday == "Nie") {
+        state.sustainabilityactions = null;
+        state.sustainabilitymotivation = null;
+    } else {
+        state.nosustainabilitymotivation = null;
+    }
+
+    return state;
+
 }
